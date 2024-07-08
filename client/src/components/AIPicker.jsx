69eb104a8b37809647a3172e577fc8ca62d63027
Deepanshi <deepanshibhardwaj99@gmail.com>
2024-07-08 23:00:47 +0530

    AI Picker

--- client/src/components/AIPicker.jsx
+++ client/src/components/AIPicker.jsx
@@ -1,8 +1,49 @@
 import React from 'react'
+import CustomButton from './CustomButton';
 
-const AIPicker = () => {
+const AIPicker=({prompt, setPrompt, genereratingImg, handleSubmit})=>{
+
   return (
-    <div>AIPicker</div>
+    <div className='aipicker-container'>
+    <textarea
+    placeholder='Ask AI.....'
+    row={5}
+    value={prompt}
+    onChange={(e)=>setPrompt(e.target.value
+    )}
+    className='aipicker-textarea'
+    />
+    <div className='flex flex-wrap gap-3'>
+    { genereratingImg ? (
+      <CustomButton
+      type="outline"
+      title="Asking AI.."
+      customStyles="text-xs"
+      />
+    
+    )  :( 
+      <>
+      <CustomButton
+      type="outline"
+      title="AI Logo"
+      handleClick={()=> handleSubmit('logo') }
+      customStyles="text-xs"
+      />
+      <CustomButton
+      type="filled"
+      title="AI Full Image"
+      handleClick={()=> handleSubmit('full') }
+      customStyles="text-xs"
+      />
+
+
+
+      </>
+
+    )}
+
+    </div>
+    </div>
   )
 }
 
--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -52,9 +52,21 @@ const Customizer = () => {
     setGeneratingImg(true);
 
     try {
-      // Assume generateImage is a function that generates an image based on the prompt
-      const result = await generateImage(prompt); // Implement generateImage as per your backend API
-      handleDecals(type, result);
+      setGeneratingImg(true);
+
+      const response= await fetch('http://localhost:8080/api/v1/dalle',{
+        method: 'POST',
+        headers:{
+          'Content-Type': 'application/json'
+        },
+        body :JSON.stringify({
+          prompt,
+        })
+      })
+
+      const data = await response.json();
+      handleDecals(type, `data:image/png;base64, ${data.photo}`)
+
     } catch (error) {
       alert(error);
     } finally {
